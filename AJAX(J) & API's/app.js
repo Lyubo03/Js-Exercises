602a0e0439ac13a943452607824069078685e36a
Lyubo03 <luboslavbalev12@gmail.com>
2021-05-08 13:52:16 +0300

    movie Browser refactored

--- AJAX(J) & API's/app.js
+++ AJAX(J) & API's/app.js
@@ -1,21 +1,22 @@
 //movie
 const form = document.querySelector('#searchForm');
+
 form.addEventListener('submit', async function(e) {
     e.preventDefault();
     deleteTitles();
     const search = form.elements.query.value;
-    const config = { params: { q: search } };
-    const request = await axios.get(`http://api.tvmaze.com/search/shows`, config);
-    displayImage(request.data);
+    const reqData = await getMovie(search);
+    displayImage(reqData);
     form.elements.query.value = '';
 })
 
 async function getMovie(movieName) {
     try {
-        const request = axios.get(`http://api.tvmaze.com/search/shows?q=${movieName}`);
+        const config = { params: { q: movieName } };
+        const request = await axios.get(`http://api.tvmaze.com/search/shows`, config);
         return request.data;
     } catch (e) {
-
+        console.log('Error', e);
     }
 }
 const displayImage = (shows) => {
@@ -24,11 +25,11 @@ const displayImage = (shows) => {
             const img = document.createElement('IMG');
             img.src = result.show.image.medium;
             document.body.append(img);
+            displayTitle(result);
         }
     }
 }
 
-delete images
 const deleteTitles = () => {
     const imgs = document.querySelectorAll('img');
     for (let img of imgs) {
